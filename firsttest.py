128e4ade4cb4fa7780c3d52e5c733afb34e28206
umairgill-4004 <umairgill.4004@gmail.com>
2026-04-28 17:08:57 +0500

    Update firsttest.py

--- firsttest.py
+++ firsttest.py
@@ -1,35 +1,17 @@
-# vim test.py
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
 options = Options()
-options.add_argument('--no-sandbox')
-options.add_argument('--headless')
-options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 
-service = Service('/usr/bin/chromedriver')  # Explicit path to chromedriver
-driver = webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome(options=options)
 
-#driver = webdriver.Chrome(options=options)
-
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-
-title = driver.title
-assert title == "Web form"
-
-driver.implicitly_wait(5)
-
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-text_box.send_keys("Selenium")
-submit_button.click()
-
-message = driver.find_element(by=By.ID, value="message")
-value = message.text
-print(value)
-
-assert value == "Received!"
-driver.quit()
+try:
+    driver.get("https://www.google.com")
+    print("Page title:", driver.title)
+finally:
+    driver.quit()
